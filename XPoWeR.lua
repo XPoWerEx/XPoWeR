8d97bd43f3a94f641d5d35c875a92c42f14c3574
XPoWerEx <93440689+XPoWerEx@users.noreply.github.com>
2021-10-30 20:37:50 +0200

    Add files via upload

--- XPoWeR.lua
+++ XPoWeR.lua
@@ -20,7 +20,7 @@ if io.open("File_Bot/commands.lua","r") then
 io.close(io.open("File_Bot/commands.lua","r"))
 print("done => commands.lua")
 else
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/XPoWerEx/Files_XPoWeR/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/XPoWerEx/XPoWeR/main/File_Bot/commands.lua") 
 print("end wget => commands.lua")
 end
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
@@ -3792,7 +3792,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if DevXPoWeRW(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/XPoWerEx/Files_XPoWeR/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/XPoWerEx/XPoWeR/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -3830,7 +3830,7 @@ t = " *✬︙الملف »* "..file.."\n*✬︙تم تعطيل ملف* \n"
 else
 t = " *✬︙بالتاكيد تم تعطيل ملف →* "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/XPoWerEx/Files_XPoWeR/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/XPoWerEx/XPoWeR/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -3850,7 +3850,7 @@ t = " *✬︙بالتاكيد تم تفعيل ملف →* "..file.." \n"
 else
 t = " *✬︙الملف »* "..file.."\n*✬︙تم تفعيل ملف* \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/XPoWerEx/Files_XPoWeR/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/XPoWerEx/XPoWeR/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
